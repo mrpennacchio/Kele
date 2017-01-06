69936aeea666a0d861a12fb2368908678db0e07f
Anthony Pennacchio <mrpennacchio@gmail.com>
2017-01-06 11:54:43 -0500

    added checkpoint-submission

--- lib/kele.rb
+++ lib/kele.rb
@@ -48,4 +48,15 @@ include HTTParty, Roadmap
       })
   end
 
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", headers: { "authorization": @auth_token },
+      body: {
+        "checkpoint_id": checkpoint_id,
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "comment": comment,
+        "enrollment_id": enrollment_id
+        })
+  end
+
 end
